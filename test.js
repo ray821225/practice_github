179147a45763ad3d6bd7872857b22a7261301bd2
ray821225 <h24172037@gmail.com>
2021-02-10 15:45:43 +0800

    add new year console

--- test.js
+++ test.js
@@ -2,13 +2,12 @@ const date = new Date();
 
 console.log(date);
 
-
 const day = date.getDay();
 
-
 console.log(day);
-
 
 const time = date.getTime();
 
 console.log(time);
+
+console.log("Happy new year");
